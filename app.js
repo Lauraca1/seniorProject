99442f965488ca89672792dfba7644da59dad04d
Laura <lcamac02@calpoly.edu>
2023-02-12 22:18:47 -0800

    working starter app

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
+const books = require('./routes/api/books');
 
 const app = express();
-//connect to database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!!'));
+// cors
+app.use(cors({ origin: true, credentials: true }));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
